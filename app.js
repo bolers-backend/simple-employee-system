db093568a005ed742f0e90d01f208f4d81003a01
Ari. <69asdfghjkl00@gmail.com>
2024-03-30 15:58:51 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import http from "http";
+
+const app = express();
+const httpServer = http.createServer(app);
+
+app.use(express.json());
+
+// TODO: create this bellow
+app.use("/employee", );
+
+
+// Testing connection
+app.get("/ping", (req, res, next) => {
+	return res.status(200).json({ping: "pong"});
+});
+
+httpServer.listen(3000, () => {
+	console.log("server start...");
+});
